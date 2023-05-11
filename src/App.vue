03440b89c7fdadf1a035851ea8eccba02fb2d8ab
Angela Ribeiro <angelaribeiro2@hotmail.com>
2023-05-11 14:41:49 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from 'vue';
+
+let num = ref(0)
+
+</script>
+
+<template>
+<!--navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+  <router-link class="navbar-brand" to="/">
+      <img src="https://raw.githubusercontent.com/yurimarcon/pokevite-preparacao/d0d020c76b0aa306f54033bd7325e1fe130287e7/src/assets/icon.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      PokeVite
+  </router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" aria-current="page" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!--content-->
+<div class="main">
+  <router-view></router-view>
+</div>
+
+<footer class="bg-primary text-light">
+<div> Desing in
+  {{ new Date().getFullYear() }}
+</div>
+</footer>
+</template>
+
+<style>
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 56px;
+}
+</style>
---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,35 @@
+<script setup>
+const pokemon = defineProps(["name", "urlBaseSvg"]);
+</script>
+
+<template>
+  <div class="col-4">
+    <div class="card p-2 mb-3 cardListPokemon">
+      <p class="text-center">{{ pokemon.name }}</p>
+      <img
+        :src="urlBaseSvg"
+        class="card-img-top"
+        alt="..."
+        height="80"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cardListPokemon {
+    background: rgb(72,63,251);
+    background: radial-gradient(circle,rgba(94,87,232,.5) 0%,rgba(70,252,244,.2) 100%);
+    cursor: pointer;
+}
+
+.cardListPokemon:hover {
+      background: rgb(72,63,251);
+    background: radial-gradient(circle,rgba(94,87,232,.6) 0%,rgba(70,252,244,.4) 100%);
+}
+
+.cardListPokemon img:hover {
+    padding: .1rem;
+
+}
+</style>
